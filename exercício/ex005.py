2e196d345afb4a198e851193fb322578b7cef32c
enzo1samuel30 <enzo.samuel.soares@escola.pr.gov.br>
2026-03-11 11:31:44 -0300

    Create ex005.py

---
+++ exercício/ex005.py
@@ -0,0 +1,4 @@
+n = input('digite algo ')
+print(n.isnumeric())#para pergutar se é um número
+print(n.isalpha())#para perguntar se é letra
+print(n.isupper())#para perguntar se é so com letras maiscilas
