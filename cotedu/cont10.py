374602089c19482d29f62538310e293c6bd706ed
enzo1samuel30 <enzo.samuel.soares@escola.pr.gov.br>
2026-03-26 08:51:05 -0300

    Create cont10.py

---
+++ cotedu/cont10.py
@@ -0,0 +1,15 @@
+importando funcionalidades específicas
+from math import sqrt, floor, ceil
+num = int(input('Digite um número: ' ))
+raiz = sqrt(num)
+print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz))) arredonda pra cima
+print('a raiz de {} é igual a {}'.format(num, floor(raiz)))
+erro
+
+importando funcionalidades específicas
+from math import sqrt, floor, ceil
+num = int(input('Digite um número: ' ))
+raiz = sqrt(num)
+print('a raiz de {} é igual a {}'.format(num, ceil(raiz))) arredonda pra cima
+print('a raiz de {} é igual a {}'.format(num, floor(raiz)))
+ok
