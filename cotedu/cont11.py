f80339938a8b7f386f08cc29fcaad23ca70c383e
enzo1samuel30 <enzo.samuel.soares@escola.pr.gov.br>
2026-05-20 10:47:40 -0300

    Create cont11.py

---
+++ cotedu/cont11.py
@@ -0,0 +1,7 @@
+Condições aninhadas
+São condições colocadas dentro de outras condições. Em condições 
+aninhadas eu utilizo o se não se, dentro do se. Em python o se não
+se é representado por elif condição: A partir do primeiro elif 
+para mais condições você vai colocando um elif para cada condição.
+Sempre que for usar o primiero elif tem que usar um if antes e o
+else se torna opcional.
